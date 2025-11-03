b62285c6b6728d4e8bb25f78e0311e965e1ea8c1
Rendy Isdwanta <risdwanta@gmail.com>
2025-11-03 18:13:11 +0700

    c2_5 additional comment

--- src/c2_5/c2_5_fasm.s
+++ src/c2_5/c2_5_fasm.s
@@ -3,6 +3,15 @@ section .text
 ; extern void mul_i32_a(int* prod1, long long* prod2, int a, int b);
 ; extern int div_i32_a(int* quo, int* rem, int a, int b);
 
+; So in short:
+; If you really need 128-bit multiplication result
+; you mush use the 1-operand imul.
+;
+; 1-operand imul is basically whatever register you pass (explicit register operand)
+; multiplied by whatever it is in RAX (implicit register operand)
+;
+; the result of 1-operand is stored in RDX:RAX high and low 64-bit respectively
+
 global mul_i32_a
 mul_i32_a:
     mov eax, edx                ; eax = a
